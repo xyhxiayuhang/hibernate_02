92692096d24a35ce6dd6863799bd2133d7f27469
xyhxiayuhang <153996626@qq.com>
2016-03-07 20:27:30 +0800

    testSaveStudentsUseOpenSession

--- test/SessionTest.java
+++ test/SessionTest.java
@@ -1,6 +1,9 @@
 
+import java.util.Date;
+
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.service.ServiceRegistry;
 import org.hibernate.service.ServiceRegistryBuilder;
@@ -39,5 +42,30 @@ public class SessionTest {
 			System.out.println("session创建成功");
 		} else
 			System.out.println("session创建失败");
+	}
+
+	@Test
+	public void testSaveStudentsUseOpenSession() {
+		Configuration config = new Configuration().configure();// 获得配置对象
+		// 获取服务注册对象
+		ServiceRegistry serviceRegistry = new ServiceRegistryBuilder().applySettings(config.getProperties())
+				.buildServiceRegistry();
+		// 获得sessionFactory对象
+		SessionFactory sessionFactory = config.buildSessionFactory(serviceRegistry);
+		// 获得session对象
+		Session session1 = sessionFactory.getCurrentSession();
+		Transaction transaction = session1.beginTransaction();
+		Students s = new Students(1, "张三", "男", new Date(), "北京");
+		session1.save(s);
+		// session1.close();
+		transaction.commit();// 提交事务
+
+		Session session2 = sessionFactory.openSession();
+		transaction = session2.beginTransaction();
+		Students s1 = new Students(2, "李四", "男", new Date(), "上海");
+		session2.save(s1);
+		transaction.commit();// 提交事务
+		// session2.close();
+
 	}
 }
